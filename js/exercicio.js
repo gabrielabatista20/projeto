4564ae0e27e38acc97fccf33f10f772765a33f54
Aluno manhã <alunomanha@senai.ti>
2020-12-22 12:24:48 -0300

    subindo projeto

---
+++ js/exercicio.js
@@ -0,0 +1,53 @@
+// exercicio
+
+var collor;
+
+console.log (typeof collor)
+
+const person ={
+     name: "Rick",
+     age: 30,
+     isAdmin: true,
+     start= 2.5
+}
+const person ={
+    name: "Rick",
+    age: 30,
+    isAdmin: true,
+    start= 2.5
+}
+
+console.log(typeof person);
+console.log(
+    name
+    Idade 
+    Qualificaçâo 
+    Administrador 
+)
+var studerts=[]
+console.log(typeof students)
+
+studerts=[
+    person
+]
+console.log(typeof studerts);
+
+console.log(studerts[0]);
+
+const morty={
+    name: morty,
+    age: 15,
+    starts: 3.6,
+    isAdmin: false
+}
+studerts=[
+    person,
+    morty
+] 
+console.log (studerts)
+console.log (studerts [0])
+console.log (studerts[1])
+
+console.log("Frase 1")
+console.log("frase 2")
+console.log("frase 3")
